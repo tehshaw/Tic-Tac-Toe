66a29c49f3c119ea40ce5b13bc20bcf6f8fbdbc3
tehshaw <25413002+tehshaw@users.noreply.github.com>
2022-02-26 00:57:14 -0500

    Add PC player AI to look for moves to block and win with

--- logic/PlayerTwo.js
+++ logic/PlayerTwo.js
@@ -2,13 +2,51 @@
 
 export const playerTwo = (grid, player) => {
 
-    const priorGird = Object.keys(grid).filter(square => grid[square] === "")
+    const gridState = [
+        ['one',   'two',    'three'],
+        ['four',  'five',   'six'],
+        ['seven', 'eight',  'nine'],
+        ['one',   'four',  'seven'],
+        ['two',   'five',   'eight'],
+        ['three', 'six',    'nine'],
+        ['one',   'five',   'nine'],
+        ['three', 'five',   'seven']
+    ]
 
-    const currentGrid = priorGird.filter(square => square != player )
+    let blockPlays = new Set()
+    let winPlays = new Set()
+    let plays = new Set()
+ 
+    for(let play of gridState){
+        let X = 0
+        let O = 0
+        let E = []
+        for(let square of play){
+            let move = grid[square]
+            move == "X" ? X++ : move == "O" ? O++ : E.push(square)
+        }
 
-    const play = Math.floor(Math.random()*currentGrid.length)
+        if(X == 2 && E.length == 1){ 
+            E.map(e => blockPlays.add(e))
+        }else if(O == 2 && E.length == 1){
+            E.map(e => winPlays.add(e))
+        }else {
+            E.map(e => plays.add(e))
+        }   
+    }
 
-    return currentGrid[play]
+    let nextPlays = [] 
+
+    winPlays.size == 0 ? 
+        blockPlays.size == 0 ? 
+            plays.forEach((value)=> nextPlays.push(value)) : 
+            blockPlays.forEach((value)=> nextPlays.push(value)) :
+            winPlays.forEach((value)=> nextPlays.push(value))
+
+    const PCplay = Math.floor(Math.random()*nextPlays.length)
+
+    console.log(nextPlays[PCplay])
+    return nextPlays[PCplay]
 
 }
 
